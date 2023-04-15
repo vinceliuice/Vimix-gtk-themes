4eae5a4fd1c0cde6767e79457ed68a6ac0bc8771
vinceliuice <vinceliuice@hotmail.com>
2023-04-15 19:03:59 +0800

    Fixed gnome-shell 44.0 issues

--- parse-sass.sh
+++ parse-sass.sh
@@ -25,6 +25,8 @@ for color in "${_COLOR_VARIANTS[@]}"; do
   echo "==> Generating the 40.0 gnome-shell${color}.css..."
   sassc $SASSC_OPT src/gnome-shell/shell-42-0/gnome-shell${color}.{scss,css}
   echo "==> Generating the 42.0 gnome-shell${color}.css..."
+  sassc $SASSC_OPT src/gnome-shell/shell-44-0/gnome-shell${color}.{scss,css}
+  echo "==> Generating the 44.0 gnome-shell${color}.css..."
   sassc $SASSC_OPT src/cinnamon/cinnamon${color}.{scss,css}
   echo "==> Generating the cinnamon${color}.css..."
 done

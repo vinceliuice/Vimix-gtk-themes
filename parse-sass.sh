07d68c6cd8edb189524cfa51f3577c76f1440d8b
vinceliuice <vinceliuice@hotmail.com>
2025-03-20 00:29:57 +0800

    Fixed issues

--- parse-sass.sh
+++ parse-sass.sh
@@ -1,8 +1,26 @@
 #! /usr/bin/env bash
 
+#  Check command avalibility
+function has_command() {
+  command -v $1 > /dev/null
+}
+
+#  Install needed packages
 if [ ! "$(which sassc 2> /dev/null)" ]; then
-   echo sassc needs to be installed to generate the css.
-   exit 1
+  echo sassc needs to be installed to generate the css.
+  if has_command zypper; then
+    sudo zypper in sassc
+  elif has_command apt; then
+    sudo apt install -y sassc
+  elif has_command dnf; then
+    sudo dnf install -y sassc
+  elif has_command yum; then
+    sudo yum install -y sassc
+  elif has_command pacman; then
+    sudo pacman -S --noconfirm sassc
+  elif has_command xbps-install; then
+    sudo xbps-install -y sassc
+  fi
 fi
 
 SASSC_OPT="-M -t expanded"
